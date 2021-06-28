3aade81e54fc5b93d9b0c08fecfb49944540778f
Raymond <70726900+ray165@users.noreply.github.com>
2021-06-28 05:00:41 -0700

    Added markers and switched to react-map-gl

---
+++ client/src/map.js
@@ -0,0 +1,78 @@
+import React, { useRef, useEffect, useState } from "react";
+import ReactMapGL, { Marker } from 'react-map-gl';
+// var mapboxgl = require("mapbox-gl/dist/mapbox-gl.js"); 
+// Switching to use react-map-gl wrapper for mapbox... made by uber for the market
+
+
+/**
+ *  Make a file called '.env.local' and put in your mapbox API key
+ * Inside .env.local should be:
+ * REACT_APP_MAPBOX_ACCESS_TOKEN=<your token goes here>
+ */
+// mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
+
+
+const cities = [
+  {
+    name: "Coquitlam",
+    longitude: -122.793205,
+    latitude: 	49.283764,
+  },
+  {
+    name: "Burnaby",
+    longitude: -122.994560,
+    latitude: 49.246445,
+  },
+  {
+    name: "Richmond",
+    longitude: -123.133568,
+    latitude: 	49.166592,
+  },
+]
+
+export default function MapPage() {
+  const mapContainer = useRef(null);
+  // lng and lat set to vancouver
+  const [lng, setLng] = useState(-123.116226);
+  const [lat, setLat] = useState(49.246292);
+  const [zoom, setZoom] = useState(10);
+
+  const [viewport, setViewport] = React.useState({
+    longitude: lng,
+    latitude: lat,
+    zoom: zoom
+  });
+
+  const markers = React.useMemo(() => cities.map(
+    city => (
+      <Marker key={city.name} longitude={city.longitude} latitude={city.latitude}>
+        <div>Check out {city.name}!! </div>
+      </Marker>
+
+    )
+  ), [cities])
+
+  // useEffect(() => {
+  //   const map = new mapboxgl.Map({
+  //     container: mapContainer.current,
+  //     style: "mapbox://styles/mapbox/streets-v11",
+  //     center: [lng, lat],
+  //     zoom: zoom
+  //   });
+
+  //   map.addControl(new mapboxgl.NavigationControl(), "bottom-right");
+
+  //   return () => {
+  //     map.remove();
+  //   };
+  // }, [lng, lat, zoom]);
+
+  return (
+    <>
+      {/* <div className="map-container" ref={mapContainer} /> */}
+      <ReactMapGL {...viewport} width="100vw" height="100vh" onViewportChange={setViewport} mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}>
+        {markers}
+      </ReactMapGL>
+    </>
+  );
+}
